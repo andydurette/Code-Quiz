b6f0237959c31ef63a59e2f3fea0e130f0bddce3
Andy Durette <duretteandy@gmail.com>
2019-10-18 12:55:01 -0400

    Cleaned up javascript querying with one unified method from a function call

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,15 +18,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 //////////////////// FUNCTIONS TO BE CALLED IN THE CODE CREATED AS THE CODE IS REPEATED HENCE FUNCTIONS ARE EFFICIENT ////////////////////
 
+  // FUNCTION to more quickly call elements less typing means less chance for errors
+  let queryElement = (element) =>{
+    return document.querySelector(element)
+  }
+
   // FUNCTION to reset HTML display for the score
   let recordsHtmlReset = () => {
-      document.querySelector('#highScores div').innerHTML = "";
+      queryElement('#highScores div').innerHTML = "";
       var i = 1;
       recordsArray.sort((a, b) => b.score - a.score);
       Array.from(recordsArray).forEach(check => {
           var scores = document.createElement("div");
           scores.innerHTML = i + ". " + check.initialRecord + " - " + check.score;
-          document.querySelector('#highScores div').appendChild(scores);
+          queryElement('#highScores div').appendChild(scores);
           i = i + 1
       });
       i = 0;
@@ -35,24 +40,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
   }
 
-
   // FUNCTION to set the question data in questionHolder section
   let setQuestionData = () => {
-      document.querySelector('#quizHolder p').innerHTML = questions[qCount].title;
-      document.querySelector('#quizHolder ol li:nth-of-type(1)').innerHTML = `1. ${questions[qCount].choices[0]}`;
-      document.querySelector('#quizHolder ol li:nth-of-type(2)').innerHTML = `2. ${questions[qCount].choices[1]}`;
-      document.querySelector('#quizHolder ol li:nth-of-type(3)').innerHTML = `3. ${questions[qCount].choices[2]}`;
-      document.querySelector('#quizHolder ol li:nth-of-type(4)').innerHTML = `4. ${questions[qCount].choices[3]}`;
+      queryElement('#quizHolder p').innerHTML = questions[qCount].title;
+      queryElement('#quizHolder ol li:nth-of-type(1)').innerHTML = `1. ${questions[qCount].choices[0]}`;
+      queryElement('#quizHolder ol li:nth-of-type(2)').innerHTML = `2. ${questions[qCount].choices[1]}`;
+      queryElement('#quizHolder ol li:nth-of-type(3)').innerHTML = `3. ${questions[qCount].choices[2]}`;
+      queryElement('#quizHolder ol li:nth-of-type(4)').innerHTML = `4. ${questions[qCount].choices[3]}`;
   }
-
 
   //call above function to set Initial data in questionHolder section
   setQuestionData();
 
   //FUNCTION changes the question and has a parameter to control the text which is provided weather it is correct or wrong
   let quizUpdate = (answerCopy) => {
-    document.querySelector('#scoreIndicator p').innerHTML = answerCopy;
-    document.querySelector('#scoreIndicator').classList.remove('invisible', scoreIndicator());
+    queryElement('#scoreIndicator p').innerHTML = answerCopy;
+    queryElement('#scoreIndicator').classList.remove('invisible', scoreIndicator());
     Array.from(answers).forEach(answer => {
         answer.classList.add('disable');
     });
@@ -60,10 +63,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // If all the questions have been answered exist the quiz section
     setTimeout(() => { 
     if (qCount === questions.length) {
-        document.getElementById('quizHolder').classList.add('hide');
-        document.getElementById('finish').classList.remove('hide');
+        queryElement('#quizHolder').classList.add('hide');
+        queryElement('#finish').classList.remove('hide');
         time = 0;
-        document.getElementById('time').innerHTML = time;
+        queryElement('#time').innerHTML = time;
     } else {
         // Updates copy in questions with the net array's question text.
         setQuestionData(); 
@@ -71,7 +74,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         Array.from(answers).forEach(answer => {
             answer.classList.remove('disable');
         });
-        //setQuestionData();
     }
   }, 1000);
 }
@@ -81,23 +83,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
 let myTimer = () => {
     if (time > 0) {
         time = time - 1;
-        document.getElementById('time').innerHTML = time;
+        queryElement('#time').innerHTML = time;
     } else {
         clearInterval(clock);
-        document.getElementById('score').innerHTML = score;
-        document.getElementById('finish').classList.remove('hide');
-        document.getElementById('quizHolder').classList.add('hide');
+        queryElement('#score').innerHTML = score;
+        queryElement('#finish').classList.remove('hide');
+        queryElement('#quizHolder').classList.add('hide');
     }
 }
-
 
 //////////////////// QUIZ TIMER ////////////////////
 
 // On intro button click start time and starts giving questions
 let clock;
-document.querySelector("#intro button").addEventListener("click", (e) => {
-    document.getElementById('intro').classList.add('hide');
-    document.getElementById('quizHolder').classList.remove('hide');
+queryElement("#intro button").addEventListener("click", (e) => {
+    queryElement('#intro').classList.add('hide');
+    queryElement('#quizHolder').classList.remove('hide');
     clock = setInterval(myTimer, 1000);
 });
 
@@ -106,7 +107,7 @@ document.querySelector("#intro button").addEventListener("click", (e) => {
 let scoreIndicator = () => {
     clearTimeout(timeset);
     timeset = setTimeout(() => {
-        document.querySelector('#scoreIndicator').classList.add('invisible');
+        queryElement('#scoreIndicator').classList.add('invisible');
     }, 1000);
 }
 
@@ -132,29 +133,27 @@ let scoreIndicator = () => {
 
 //////////////////// SCORE SUBMISSION ////////////////////
 
-
 // Displays error message if initials given do not meet requirments
   let errorIndicator = () => {
     clearTimeout(timeset);
     timeset = setTimeout(() => {
-        document.querySelector('#errorIndicator').classList.add('invisible');
+        queryElement('#errorIndicator').classList.add('invisible');
     }, 3000);
 }
 
-
 // Error handling for submitting high scores
-  document.querySelector("#records button").addEventListener("click", () => {
+queryElement("#records button").addEventListener("click", () => {
 
-      let initialsRecord = document.querySelector('#initials').value;
+      let initialsRecord = queryElement('#initials').value;
       if (initialsRecord === '') {
-          document.querySelector('#errorIndicator p').innerHTML = "You need at least 1 character";
-          document.querySelector('#errorIndicator').classList.remove('invisible', errorIndicator());
+          queryElement('#errorIndicator p').innerHTML = "You need at least 1 character";
+          queryElement('#errorIndicator').classList.remove('invisible', errorIndicator());
       } else if (initialsRecord.match(/[[A-Za-z]/) === null) {
-          document.querySelector('#errorIndicator p').innerHTML = "Only letters for initials allowed.";
-          document.querySelector('#errorIndicator').classList.remove('invisible', errorIndicator());
+          queryElement('#errorIndicator p').innerHTML = "Only letters for initials allowed.";
+          queryElement('#errorIndicator').classList.remove('invisible', errorIndicator());
       } else if (initialsRecord.length > 5) {
-          document.querySelector('#errorIndicator p').innerHTML = "Maximum of 5 characters allowed.";
-          document.querySelector('#errorIndicator').classList.remove('invisible', errorIndicator());
+          queryElement('#errorIndicator p').innerHTML = "Maximum of 5 characters allowed.";
+          queryElement('#errorIndicator').classList.remove('invisible', errorIndicator());
       } else {
           //Sends value to current array for use now.
           recordsArray.push({
@@ -163,27 +162,25 @@ let scoreIndicator = () => {
           });
           //Sends value to local storage for later use.
           localStorage.setItem('recordsArray', JSON.stringify(recordsArray));
-          document.querySelector('#highScores div').innerHTML = '';
-          document.getElementById('finish').classList.add('hide');
-          document.getElementById('highScores').classList.remove('hide');
+          queryElement('#highScores div').innerHTML = '';
+          queryElement('#finish').classList.add('hide');
+          queryElement('#highScores').classList.remove('hide');
           recordsHtmlReset();
-          document.querySelector("#initials").value = '';
+          queryElement("#initials").value = '';
       }
   });
 
-
 //////////////////// HIGH SCORE CONTROL ARRAY/LOCAL STORAGE ////////////////////
 
-
   // Clears highscores from the html, array and localstorage
-  document.querySelector("#clearScores").addEventListener("click", () => {
+  queryElement("#clearScores").addEventListener("click", () => {
       recordsArray = [];
-      document.querySelector('#highScores div').innerHTML = "";
+      queryElement('#highScores div').innerHTML = "";
       localStorage.removeItem('recordsArray');
   });
 
   // Resets all quiz settings to the default to replay the quiz
-  document.querySelector("#reset").addEventListener("click", () => {
+  queryElement("#reset").addEventListener("click", () => {
       time = initialTime;
       score = 0;
       qCount = 0;
@@ -192,14 +189,14 @@ let scoreIndicator = () => {
       Array.from(sections).forEach((userItem) => {
           userItem.classList.add('hide');
       });
-      document.getElementById('intro').classList.remove('hide');
+      queryElement('#intro').classList.remove('hide');
   });
 
   // If a player pushes the view high scores button in the html view then this abdandons all quiz progress and lets them view the high scores.
-  document.getElementById("scores").addEventListener("click", (e) => {
+  queryElement("#scores").addEventListener("click", (e) => {
       e.preventDefault();
       clearInterval(clock);
-      document.getElementById('time').innerHTML = 0;
+      queryElement('#time').innerHTML = 0;
       time = initialTime;
       score = 0;
       qCount = 0;
@@ -208,9 +205,8 @@ let scoreIndicator = () => {
       Array.from(sections).forEach((userItem) => {
           userItem.classList.add('hide');
       });
-      document.getElementById('highScores').classList.remove('hide');
+      queryElement('#highScores').classList.remove('hide');
       recordsHtmlReset();
   });
-
 
 });
